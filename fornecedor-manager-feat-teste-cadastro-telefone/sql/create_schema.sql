e86dd3b4968a8534ba3e76e1324745934af501d7
Thaís Breder <thais.breder@zup.com.br>
2023-12-05 17:52:49 -0300

    Delete fornecedor-manager-feat-teste-cadastro-telefone directory

--- fornecedor-manager-feat-teste-cadastro-telefone/sql/create_schema.sql
+++
@@ -1,1 +0,0 @@
-create schema fornecedor_test;
--- fornecedor-manager-feat-teste-cadastro-telefone/src/main/java/br/com/zup/edu/fornecedormanager/repository/TelefoneRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.zup.edu.fornecedormanager.repository;
-
-import br.com.zup.edu.fornecedormanager.model.Telefone;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TelefoneRepository extends JpaRepository<Telefone,Long> {
-}
